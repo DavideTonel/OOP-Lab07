47c3b1d2b1954bc6f6080004b4fc69091a3bdbeb
Davide Tonelli <davide.tonelli8@studio.unibo.it>
2021-11-11 18:11:15 +0100

    Test passed

--- src/it/unibo/oop/lab/enum1/TestSportByEnumeration.java
+++ src/it/unibo/oop/lab/enum1/TestSportByEnumeration.java
@@ -1,5 +1,7 @@
 package it.unibo.oop.lab.enum1;
 
+import it.unibo.oop.lab.socialnetwork.User;
+
 /**
  * This is going to act as a test for
  * {@link it.unibo.oop.lab.enum1.SportSocialNetworkUserImpl}
@@ -25,5 +27,12 @@ public final class TestSportByEnumeration {
          * By now, you know how to do it
          */
         // TODO
+    	final SportSocialNetworkUserImpl<User> falonso = new SportSocialNetworkUserImpl<User>("Fernando", "Alonso", "falonso", 34);
+        
+    	falonso.addSport(Sport.SOCCER);
+        falonso.addSport(Sport.F1);
+        falonso.addSport(Sport.BIKE);
+        System.out.println("Alonso practices F1: " + falonso.hasSport(Sport.F1));
+        System.out.println("Alonso does not like volley: " + !falonso.hasSport(Sport.VOLLEY));
     }
 }
